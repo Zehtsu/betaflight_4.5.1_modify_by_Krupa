d4008b208cd8c51be56cfcf7c08d1e0cd240dc89
Zehtsu <125655948+Zehtsu@users.noreply.github.com>
2026-09-20 19:16:39 +0300

    Update vtx_common.h

--- src/main/drivers/vtx_common.h
+++ src/main/drivers/vtx_common.h
@@ -29,7 +29,7 @@
 #include "common/streambuf.h"
 
 #define VTX_SETTINGS_MIN_FREQUENCY_MHZ 5000          //min freq (in MHz) for 'vtx_freq' setting
-#define VTX_SETTINGS_MAX_FREQUENCY_MHZ 5999          //max freq (in MHz) for 'vtx_freq' setting
+#define VTX_SETTINGS_MAX_FREQUENCY_MHZ 7500          //max freq (in MHz) for 'vtx_freq' setting
 
 #if defined(USE_VTX_RTC6705)
 
